b937e7a1660e679031adbb489c5ec236a6a625f0
AleKs <alexanderfranciscog@gmail.com>
2023-07-25 15:29:34 -0400

    renamed HelloApplication to TicketManiaApp

--- src/main/java/com/example/ticketmania/TicketManiaApp.java
+++ src/main/java/com/example/ticketmania/TicketManiaApp.java
@@ -4,15 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TicketManiaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader root = new FXMLLoader(TicketManiaApp.class.getResource("login.fxml"));
+        Scene scene = new Scene(root.load(), 1280, 720);
+        stage.setTitle("TicketMania");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/ticketmania/TicketManiaController.java
@@ -0,0 +1,40 @@
+package com.example.ticketmania;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+    @FXML
+    private BorderPane login;
+
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    private PasswordField pass;
+
+    @FXML
+    private ImageView brand;
+
+    @FXML
+    private void handleSubmit(ActionEvent event) {
+        String username = user.getText();
+        String password = pass.getText();
+
+        if (isValidUser(username, password)) {
+            System.out.println("Inicio de sesión exitoso");
+        } else {
+            System.out.println("Credenciales inválidas. Inténtalo de nuevo.");
+        }
+    }
+
+    private boolean isValidUser(String username, String password) {
+        return username.equals("user") && password.equals("pass");
+    }
+}
