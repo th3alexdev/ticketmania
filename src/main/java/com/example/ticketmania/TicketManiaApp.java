125637601a241dcb315027f22478a561a2381c33
AleKs <alexanderfranciscog@gmail.com>
2023-07-29 23:39:15 -0400

    adjust initial scene dimensions

--- src/main/java/com/example/ticketmania/TicketManiaApp.java
+++ src/main/java/com/example/ticketmania/TicketManiaApp.java
@@ -5,12 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-
 public class TicketManiaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader root = new FXMLLoader(TicketManiaApp.class.getResource("login.fxml"));
-        Scene scene = new Scene(root.load(), 1280, 720);
+        Scene scene = new Scene(root.load(), 1100, 740);
         stage.setTitle("TicketMania");
         stage.setScene(scene);
         stage.show();
