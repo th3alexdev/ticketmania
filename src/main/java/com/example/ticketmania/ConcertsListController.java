31a145d75fd98e985787537ccaf739f209be1e19
AleKs <alexanderfranciscog@gmail.com>
2023-07-30 23:26:11 -0400

    add Concert data + fixed instances of the Concert class

--- src/main/java/com/example/ticketmania/ConcertsListController.java
+++ src/main/java/com/example/ticketmania/ConcertsListController.java
@@ -24,19 +24,74 @@ public class ConcertsListController {
     @FXML
     private Stage stage;
 
-
     private List<Concert> allConcerts;
-    private int concertId = -1;
+    public Concert selectedConcert = null;
+    public int concertId = -1;
+
+
     public ConcertsListController() {
         allConcerts = new ArrayList<>();
         Concert concert1 = new Concert(
                 "La Última Misión Tour",
                 "Wisin y Yandel",
-                "12 AGOS",
+                12,
+                "AGOS",
                 "Sábado 12 Agosto",
                 "07:00 PM",
-                "..\\assets\\events\\Wisin y Yandel_3.jpg");
+                "/com/example/assets/events/wisin_y_yandel.png",
+                      new int[]{1, 2, 3, 4, 5, 6}); // areas
+        Concert concert2 = new Concert(
+                "Indeleble",
+                "Los Mesoneros",
+                18,
+                "AGOS",
+                "Viernes 18 Agosto",
+                "04:00 PM",
+                "/com/example/assets/events/los_mesoneros.png",
+                      new int[]{1, 2, 3, 4, 5, 6}); // areas
+        Concert concert3 = new Concert(
+                "Tour Me Verás Volver",
+                "Soda Stereo",
+                1,
+                "SEP",
+                "Viernes 1 Septiembre",
+                "08:15 PM",
+                "/com/example/assets/events/soda_stereo.png",
+                       new int[]{1, 2, 3, 4, 5, 6}); // areas
+        Concert concert4 = new Concert(
+                "Vivir Mi Vida",
+                "Mark Anthony",
+                13,
+                "AGOS",
+                "Domingo 13 Agosto",
+                "07:00 PM",
+                "/com/example/assets/events/mark_anthony.png",
+                        new int[]{1, 2, 3, 4, 5, 6}); // areas
+        Concert concert5 = new Concert(
+                "En Cambio Tour 2023",
+                "Reik",
+                4,
+                "AGOS",
+                "Viernes 4 Agosto",
+                "07:00 PM",
+                "/com/example/assets/events/reik.png",
+                         new int[]{1, 2, 3, 4, 5, 6}); // areas
+        Concert concert6 = new Concert(
+                "LA CU4RTA HOJA",
+                "Pablo Alborán",
+                18,
+                "AGOS",
+                "Viernes 18 Agosto",
+                "07:00 PM",
+                "/com/example/assets/events/pablo_alboran.png",
+                         new int[]{1, 2, 3, 4, 5, 6}); // areas
+
         allConcerts.add(concert1);
+        allConcerts.add(concert2);
+        allConcerts.add(concert3);
+        allConcerts.add(concert4);
+        allConcerts.add(concert5);
+        allConcerts.add(concert6);
     }
 
     @FXML
@@ -71,11 +126,16 @@ public class ConcertsListController {
         }
     }
 
+    public void resetVariables() {
+        selectedConcert = null;
+        concertId = -1;
+    }
 
     @FXML
     public void moveNextPage(ActionEvent event) {
-
-        Concert selectedConcert = null;
+        selectedConcert = null;
+        System.out.println(selectedConcert);
+        System.out.println(concertId);
 
         for (Concert concert : allConcerts) {
             if (concert.getId() == concertId) {
